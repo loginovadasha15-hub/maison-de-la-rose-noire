17c6d8fbf63e58ef3e8bcabc99358ed370fc981a
loginovadasha15-hub <loginova.dasha15@gmail.com>
2026-08-01 15:28:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const seal = document.getElementById("seal");
+const letter = document.getElementById("letter");
+const accept = document.getElementById("accept");
 
+seal.addEventListener("click", () => {
+
+    seal.classList.add("fade");
+
+    setTimeout(() => {
+
+        seal.style.display = "none";
+        letter.style.display = "block";
+
+    }, 600);
+
+});
+
+accept.addEventListener("click", () => {
+
+    accept.disabled = true;
+
+    accept.innerText = "Миссия принята";
+
+    localStorage.setItem("missionAccepted","true");
+
+});
